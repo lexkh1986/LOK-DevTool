e6453be5edd05ec0627507ce4f3d84584f354e5e
Lex Khuat <lexkhuat@gmail.com>
2022-04-11 22:52:15 +0700

    FIx bug

--- src/components/RptLandContribution.js
+++ src/components/RptLandContribution.js
@@ -1,73 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import {
     ButtonGroup, Button,
     Row, Col,
     Table
 } from 'reactstrap';
+import { useCSVDownloader } from 'react-papaparse';
 import { useNavigate } from 'react-router-dom';
 
 const RptLandContribution = () => {
+    const [isRptRendered, toggleRender] = useState(false);
+    const { CSVDownloader } = useCSVDownloader();
     const navigate = useNavigate();
 
-    return (
-        <div className='land-contribution'>
-            <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
-                <h2>Lands Contributions</h2>
-                <Button outline size='sm'>
-                    Export
-                </Button>
-            </div>
-            <Row>
-                <Col md='4'>
-                    <ButtonGroup>
-                        <Button outline color='primary'
-                            onClick={() =>
-                                ReactDOM.render(<ReportTable
-                                    lands={JSON.parse(localStorage.getItem('landData'))}
-                                    members={JSON.parse(localStorage.getItem('members'))}
-                                />, document.getElementById('contributions'))
-                            }
-                            title='Click this button to generate a table of devPoint data'>
-                            <i className='fa fa-table' aria-hidden="true"></i>Generate
-                        </Button>
-                        <Button outline color='primary' id="btnSumDevPoint"
-                            onClick={() => {
-                                if (!localStorage.getItem('landContribution')) {
-                                    alert('Please generate land contribution report first!');
-                                } else {
-                                    navigate('/dashboard/report');
-                                }
-                            }}
-                            title='Group data by discordIDs'>
-                            <i className='fa fa-line-chart' aria-hidden="true"></i>Payout Report
-                        </Button>
-                    </ButtonGroup>
-                </Col>
-            </Row>
-            <Row>
-                <Col id='contributions' md='12'>
-                </Col>
-            </Row>
-        </div>
-    );
-};
-
-const ReportTable = ({ lands, members }) => {
     useEffect(() => {
         localStorage.removeItem('landContribution');
     }, []);
 
-    const genHeaders = () => {
+    const genData = () => {
+        const members = JSON.parse(localStorage.getItem('members'));
+        const lands = JSON.parse(localStorage.getItem('landData'));
+
+        if (!members) {
+            toggleRender(false);
+            return <p className='error-text'>Could not detect any members to generate report!</p>;
+        }
+
+        if (!lands) {
+            toggleRender(false);
+            return <p className='error-text'>Please get data at least 1 land!</p>;
+        }
+
         // Build headers
         let headers = ['no', 'discord', 'kingdom', 'total'];
         lands.forEach(land => {
             headers.push(land.id);
         });
-        return headers;
-    };
 
-    const genBody = () => {
         // Build body
         let body = [];
         let count = 1;
@@ -95,38 +64,84 @@ const ReportTable = ({ lands, members }) => {
         });
 
         localStorage.setItem('landContribution', JSON.stringify(body));
-        return body;
+        toggleRender(true);
+        return <ReportTable headers={headers} body={body} />;
     };
 
     return (
-        <div className='contributions-data'>
-            {!members ?
-                <p className='error-text'>Could not detect any members to calculate. Please check your member management section first!</p> :
-                !lands ?
-                    <p className='error-text'>Please get data at least 1 land first</p> :
-                    <Table size='sm' responsive hover>
-                        <thead>
-                            <tr>
-                                {
-                                    genHeaders().map((item, key) =>
-                                        <th key={key}>
-                                            {isNaN(item) ? null : <i className='fa fa-map' aria-hidden='true'></i>}
-                                            {item}
-                                        </th>)
+        <div className='land-contribution'>
+            <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
+                <h2>Lands Contributions</h2>
+                {
+                    !isRptRendered ? null :
+                        <CSVDownloader
+                            filename='LandContribution_Report'
+                            bom={true}
+                            config={{ delimeter: ',' }}
+                            download={true}
+                            data={JSON.parse(localStorage.getItem('landContribution'))}
+                        >
+                            <Button outline size='sm'>Export</Button>
+                        </CSVDownloader>
+                }
+            </div>
+            <Row>
+                <Col md='4'>
+                    <ButtonGroup>
+                        <Button outline color='primary'
+                            onClick={() => {
+                                ReactDOM.render(genData(), document.getElementById('contributions'))
+                            }}
+                            title='Click this button to generate a table of devPoint data'>
+                            <i className='fa fa-table' aria-hidden="true"></i>Generate
+                        </Button>
+                        <Button outline color='primary' id="btnSumDevPoint"
+                            onClick={() => {
+                                if (!localStorage.getItem('landContribution')) {
+                                    alert('Please generate land contribution report first!');
+                                } else {
+                                    navigate('/dashboard/report');
                                 }
+                            }}
+                            title='Group data by discordIDs'>
+                            <i className='fa fa-line-chart' aria-hidden="true"></i>Payout Report
+                        </Button>
+                    </ButtonGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col id='contributions' md='12'>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+const ReportTable = ({ headers, body }) => {
+    return (
+        <div className='contributions-data'>
+            <Table size='sm' responsive hover>
+                <thead>
+                    <tr>
+                        {
+                            headers.map((item, key) =>
+                                <th key={key}>
+                                    {isNaN(item) ? null : <i className='fa fa-map' aria-hidden='true'></i>}
+                                    {item}
+                                </th>)
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        body.map((item, key) =>
+                            <tr key={key}>
+                                {headers.map((col, key) => <td key={key}>{item[col]}</td>)}
                             </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                genBody().map((item, key) =>
-                                    <tr key={key}>
-                                        {genHeaders().map((col, key) => <td key={key}>{item[col]}</td>)}
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-                    </Table>
-            }
+                        )
+                    }
+                </tbody>
+            </Table>
         </div>
     );
 }
