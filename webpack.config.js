d92a6a5dc2112e5377e5849e2660ef26240ad47f
Lex Khuat <lexkhuat@gmail.com>
2022-04-13 08:17:12 +0700

    Update apex domain

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: "/LOK-DevTool/",
+        publicPath: "/",
         filename: 'index.bundle.js',
     },
     devServer: {
